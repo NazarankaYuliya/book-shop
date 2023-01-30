ce2a4403fa78a4e10add30bd18ea23fff4cbf9c7
juliya <juliya@MacBook-Air-juliya.local>
2023-01-30 13:14:08 +0100

    feat: add books with info

--- pages/main/mainScript.js
+++ pages/main/mainScript.js
@@ -54,10 +54,73 @@ container.append(catalogContent)
 
 //--- доступ к json данным ---
 
-fetch('../books.json')
+fetch('books.json')
   .then((response) => {
     return response.json()
   })
   .then((data) => {
     showBook(data)
   })
+
+// формирование карточки товара с данными
+function showBook(data) {
+  let fragment = new DocumentFragment()
+  let bookEl = document.querySelector('.catalogContent')
+
+  for (let el = 0; el < data.length; el++) {
+    let book = document.createElement('div')
+    book.classList.add('book')
+    book.setAttribute('data-id', el + 1)
+
+    let bookImg = document.createElement('div')
+    bookImg.classList.add('bookImg')
+    book.append(bookImg)
+
+    let image = document.createElement('img')
+    image.classList.add('image')
+    image.setAttribute('draggable', 'true')
+    image.src = data[el].imageLink
+    bookImg.append(image)
+
+    let bookContent = document.createElement('div')
+    bookContent.classList.add('bookContent')
+    book.append(bookContent)
+
+    let author = document.createElement('h5')
+    author.classList.add('author')
+    author.innerText = data[el].author
+    bookContent.append(author)
+
+    let title = document.createElement('h3')
+    title.classList.add('title')
+    title.innerText = data[el].title
+    bookContent.append(title)
+
+    let price = document.createElement('div')
+    price.classList.add('bookPrice')
+    price.innerHTML = `<p>Price: $ <span class='price'>${data[el].price}</span></p>`
+    bookContent.append(price)
+    let showMore = document.createElement('button')
+    showMore.classList.add('showMore', 'myBtn')
+    showMore.innerText = 'Show More'
+    bookContent.append(showMore)
+
+    let addToBag = document.createElement('button')
+    addToBag.classList.add('addToCart', 'myBtn')
+    addToBag.innerText = 'Add To Cart'
+    bookContent.append(addToBag)
+
+    let buttons = document.createElement('div')
+    buttons.classList.add('buttons')
+    buttons.append(showMore, addToBag)
+    bookContent.append(buttons)
+
+    showMore.addEventListener('click', () =>
+      openModal(data[el].title, data[el].description)
+    )
+
+    fragment.append(book)
+  }
+
+  catalogContent.append(fragment)
+}
